1f2ef9aeb401247825ced0d48dfd187f9e6b0d6f
Keshav Bhati <keshavbhati2000@gmail.com>
2022-09-21 13:16:59 -0400

    commit tree ff6012b9461e19e849d454b3a395cb635a0aa970

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const test1 = document.querySelector('#main');
+test1.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id="victory";
+
+newHeader.textContent="Rick is the champion";
+
+
